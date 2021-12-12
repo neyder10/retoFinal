c45f15a80da7d08eb44187753abfa50387a15feb
neyder10 <neyder.gomez@eia.edu.co>
2021-12-12 18:09:20 -0500

    Reto Final

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'retoFinal'`, () => {
+  it(`should have as title 'retofinal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('retoFinal');
+    expect(app.title).toEqual('retofinal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('retoFinal app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('retofinal app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './master/footer/footer.component';
+import { NavbarComponent } from './master/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
